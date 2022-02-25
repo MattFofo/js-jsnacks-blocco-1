5621680e8b3d9cb8eac8531dbb99efc9abf36465
MattFofo <matteo.segattini2@gmail.com>
2022-02-25 15:46:59 +0100

    snack 3

--- script.js
+++ script.js
@@ -13,21 +13,62 @@
 //     console.log(num2);
 // }
 
-
+////////////////////////////////////////////////////////////////////////
 
 // js.snack - 2
 
-const word1 = prompt('inserisci una parola');
-const word2 = prompt("inserisci un'altra parola");
+// const word1 = prompt('inserisci una parola');
+// const word2 = prompt("inserisci un'altra parola");
 
-if (word1.length > word2.length) {
-    console.log(`la parola ${word1} è più grande`);
+// if (word1.length > word2.length) {
+//     console.log(`la parola ${word1} è più grande`);
 
-}else if (word1.length == word2.length) {
-    console.log(`la parola ${word1} e la parola ${word2} sono lunghe uguali`);
+// }else if (word1.length == word2.length) {
+//     console.log(`la parola ${word1} e la parola ${word2} sono lunghe uguali`);
         
 
-}else {
-    console.log(`la parola ${word2} è più lunga`);
+// }else {
+//     console.log(`la parola ${word2} è più lunga`);
 
+// }
+
+
+///////////////////////////////////////////////////////////////////////////
+
+
+// js.snack - 3
+
+// let arrNumUser = [];
+
+// for (let i = 0; i < 10; i++) {
+
+//     const numUser = parseInt(prompt('inserisci numero'));
+//     arrNumUser.push(numUser);
+    
+    
+// }
+
+// let add = 0;
+
+// for (let i = 0; i < arrNumUser.length; i++) {
+
+//     add = add + arrNumUser[i];
+// }
+
+// console.log(add);
+
+
+// metodo senza arrey
+
+let add = 0;
+
+for (let i = 0; i < 10; i++) {
+
+    const numUser = parseInt(prompt('inserisci numero'));
+    add = add + numUser;
+    
+    
 }
+
+
+console.log(add);
