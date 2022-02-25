518fa5adf714c6445daf3b822e25a1aa9d126ea0
MattFofo <matteo.segattini2@gmail.com>
2022-02-25 15:13:20 +0100

    snack js 1

---
+++ script.js
@@ -0,0 +1,14 @@
+// js.snack - 1
+
+const num1 = parseInt(prompt('inserisci un numero'));
+const num2 = parseInt(prompt('inserisci un numero'));
+
+if (num1 > num2) {
+    console.log(num1);
+
+}else if (num1 == num2) {
+    console.log('i numeri sono uguali');
+
+}else {
+    console.log(num2);
+}
