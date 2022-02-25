6709615b4d287f10edec5b84d5bdc84d33309f1d
MattFofo <matteo.segattini2@gmail.com>
2022-02-25 15:23:50 +0100

    snack 2

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 // js.snack - 1
 
-const num1 = parseInt(prompt('inserisci un numero'));
-const num2 = parseInt(prompt('inserisci un numero'));
+// const num1 = parseInt(prompt('inserisci un numero'));
+// const num2 = parseInt(prompt('inserisci un numero'));
 
-if (num1 > num2) {
-    console.log(num1);
+// if (num1 > num2) {
+//     console.log(num1);
 
-}else if (num1 == num2) {
-    console.log('i numeri sono uguali');
+// }else if (num1 == num2) {
+//     console.log('i numeri sono uguali');
+
+// }else {
+//     console.log(num2);
+// }
+
+
+
+// js.snack - 2
+
+const word1 = prompt('inserisci una parola');
+const word2 = prompt("inserisci un'altra parola");
+
+if (word1.length > word2.length) {
+    console.log(`la parola ${word1} è più grande`);
+
+}else if (word1.length == word2.length) {
+    console.log(`la parola ${word1} e la parola ${word2} sono lunghe uguali`);
+        
 
 }else {
-    console.log(num2);
+    console.log(`la parola ${word2} è più lunga`);
+
 }
